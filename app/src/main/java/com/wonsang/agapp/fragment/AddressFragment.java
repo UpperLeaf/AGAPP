10f75d581a7fec28914ab330ecd51db69db53c6c
UpperLeaf <tkdduq8260@gmail.com>
2020-12-29 17:53:51 +0900

    Feat : Tab Layout

---
+++ app/src/main/java/com/wonsang/agapp/fragment/AddressFragment.java
@@ -0,0 +1,30 @@
+package com.wonsang.agapp.fragment;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.fragment.app.Fragment;
+
+import com.wonsang.agapp.R;
+
+public class AddressFragment extends Fragment {
+
+    private TextView textView;
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.address_fragment, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        this.textView = view.findViewById(R.id.textview);
+    }
+}
